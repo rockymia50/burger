34a39e2b4d9bc37330c175c8484fefc8ef6cc37d
Roger McIntosh <rogermcintosh@Rogers-MacBook-Air.local>
2018-03-22 13:31:33 -0400

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,3 @@
-/*
-
-To run this file, we do the following in our Terminal:
-
-1. Go to the directory of this sql file.
-
-2. Get into our mysql console.
-
-3. Run "source schema.sql"
-
-*/
 
 -- Create the database burger_db and specified it for use.
 CREATE DATABASE burger_db;
@@ -22,9 +11,6 @@ burger varchar(255) NOT NULL,
 PRIMARY KEY (id)
 );
 
---Insert a set of records.
 
 
--- INSERT INTO wishes (wish) VALUES ('Shaan wants to read minds.');
--- INSERT INTO wishes (wish) VALUES ('John wins the lottery.');
--- INSERT INTO wishes (wish) VALUES ('Kelly wishes for a room full of kittens.');
+
