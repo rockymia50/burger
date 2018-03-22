500f311a2d2200ad8d57d3c802a0d314d59f03cb
Roger McIntosh <rogermcintosh@Rogers-MacBook-Air.local>
2018-03-22 09:40:39 -0400

    connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,23 @@
 
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnectiop(process.env.JAWSDB_URL);
+   }  else{
+
+ connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "burger_db"
 });
+};
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
+console.log(connection);
 
 // Export connection for our ORM to use.
 module.exports = connection;
